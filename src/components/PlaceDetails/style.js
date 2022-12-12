ead4e47b3e6a123a97971002932f14fe07fd58d4
Sahib-Aujla <sahibpreet568@gmail.com>
2022-12-12 11:06:40 -0500

    uploading jattadvisors app on github (made in september 2022)

---
+++ src/components/PlaceDetails/style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  chip: {
+    margin: '10px 10px 10px 10px',
+  },
+  subtitle: {
+    display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: '10px', margin:'5px',
+  },
+  spacing: {
+    display: 'flex', alignItems: 'center', justifyContent: 'space-between' , margin:'5px',
+  },
+}));
